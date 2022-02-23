565c0277b6cee15a07172a536bbc6b619cbdde4a
maxgallup <m.gallup@student.vu.nl>
2022-02-23 20:20:58 +0100

    added basics

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,33 @@
+#[macro_use] extern crate rocket;
+
+use rocket_contrib::serve::StaticFiles;
 
 
-fn main() {
-    println!("Hello, world!");
+#[get("/")]
+fn world() -> &'static str {
+    "Hello, world!"
 }
 
+#[get("/")]
+fn hello() -> String {
+    format!("Hello page!")
+}
 
+#[get("/<name>")]
+fn hello_name(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[catch(404)]
+fn not_found() -> String {
+    format!("oy m8")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![world])
+        .mount("/hello", routes![hello, hello_name])
+        .register("/", catchers![not_found])
+
+}
