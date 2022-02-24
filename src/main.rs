566f65f874787829d5d569399092b157f1169e6c
maxgallup <m.gallup@student.vu.nl>
2022-02-24 22:44:29 +0100

    added markdown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+extern crate markdown;
+
+
+use std::path::PathBuf;
+use std::fs;
+
+
+#[get("/<file..>")]
+fn index(file: PathBuf) -> String {
+
+    println!("{:?}", file.to_str());
+
+    let content = fs::read_to_string(file.as_path())
+                    .expect("__unable to open file__");
+
+    let html : String = markdown::to_html(&content);
+    html
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
